0f7391635b6f6f92a270b4fedcb2975dc2ae6383
Md. Nasim Ahmed <mdnasimahmed341@gmail.com>
2023-01-16 06:29:00 -0500

    check authorization

---
+++ middlewares/auth.js
@@ -0,0 +1,27 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/user.js");
+
+exports.requireSignin = (req, res, next) => {
+    try {
+        const decoded = jwt.verify(
+            req.headers.authorization, process.env.JWT_SECRET
+        );
+        req.user = decoded;
+        next();
+    } catch (error) {
+        return res.status(401).json(error)
+    }
+}
+
+exports.isAdmin = async(req, res, next) => {
+    try {
+        const user = await User.findById(req.user._id);
+        if (user.role !== 1) {
+            return res.status(401).send("Unathosrized");
+        } else {
+            next();
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const { register, login } = require("../controllers/auth.js")
+const { register, login } = require("../controllers/auth.js");
+const { requireSignin } = require("../middlewares/auth.js");
 
 router.post("/register", register)
 router.post("/login", login)
+
+router.get("/auth-check", requireSignin, (req, res) => {
+    res.json({ ok: true })
+})
+router.gte("/admin-check", requireSignin, isAdmin, (req, res) => {
+    res.json({ ok: true })
+})
+
 module.exports = router;
