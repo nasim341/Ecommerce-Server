73c9b3d43ae7de135ec93afdad439f2b6af843c8
Md. Nasim Ahmed <mdnasimahmed341@gmail.com>
2023-01-16 11:09:31 -0500

    check middleware authorization

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { register, login } = require("../controllers/auth.js");
-const { requireSignin } = require("../middlewares/auth.js");
+const { requireSignin, isAdmin } = require("../middlewares/auth.js");
 
 router.post("/register", register)
 router.post("/login", login)
@@ -9,7 +9,7 @@ router.post("/login", login)
 router.get("/auth-check", requireSignin, (req, res) => {
     res.json({ ok: true })
 })
-router.gte("/admin-check", requireSignin, isAdmin, (req, res) => {
+router.get("/admin-check", requireSignin, isAdmin, (req, res) => {
     res.json({ ok: true })
 })
 
