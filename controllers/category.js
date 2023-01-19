38186a1c8244a82d37fd6777beac4defe3590847
Md. Nasim Ahmed <mdnasimahmed341@gmail.com>
2023-01-19 12:47:44 -0500

    create controller category

---
+++ controllers/category.js
@@ -0,0 +1,20 @@
+const Category = require("../models/category.js")
+const slugify = require("slugify");
+
+exports.create = async(req, res) => {
+    try {
+        const { name } = req.body;
+        if (!name.trim()) {
+            return res.js({ error: "Name is required" });
+        }
+        const existingCategory = await Category.findOne({ name });
+        if (existingCategory) {
+            return res.json({ error: "Already exists" });
+        }
+        const category = await new Category({ name, slug: slugify(name) }).save();
+        res.json(category);
+    } catch (error) {
+        console.log(error);
+        return res.status(400).json(error)
+    }
+};
